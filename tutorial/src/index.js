2219f74e00008f42aa8cc750616a0267f53ba395
Bhatia, Ronak K <ronak.k.bhatia@accenture.com>
2022-01-06 00:28:07 -0800

    Updating code with spread operator

--- tutorial/src/index.js
+++ tutorial/src/index.js
@@ -15,49 +15,65 @@ import "./index.css";
 // 3) We need to use camelCase for HTML attribute.
 
 // Vars set-up
-const firstBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
-  title: "I Love You to the Moon and Back",
-  author: "Amelia Hepworth",
-};
-
-const secondBook = {
-  img: "https://images-na.ssl-images-amazon.com/images/I/51ZSpMl1-LL._SX379_BO1,204,203,200_.jpg",
-  title: "Designing Data Intensive Applications in Python",
-  author: "Martin Kleppmann",
-};
-
+const books = [
+  {
+    id: 1,
+    img: "https://images-na.ssl-images-amazon.com/images/I/81eB%2B7%2BCkUL._AC_UL200_SR200,200_.jpg",
+    title: "I Love You to the Moon and Back",
+    author: "Amelia Hepworth",
+  },
+  {
+    id: 2,
+    img: "https://images-na.ssl-images-amazon.com/images/I/51ZSpMl1-LL._SX379_BO1,204,203,200_.jpg",
+    title: "Designing Data Intensive Applications in Python",
+    author: "Martin Kleppmann",
+  },
+];
 // const author = "Martin Kleppmann";
 // const title = "Designing Data Intensive Applications in Python";
 // const img =
 //   "https://images-na.ssl-images-amazon.com/images/I/51ZSpMl1-LL._SX379_BO1,204,203,200_.jpg";
 
 // You're passing props into the Booklist
+// In JS, you use map to deal with the data.
+
+// This method uses book as an object prop.
+// function BookList() {
+//   return (
+//     <section className="booklist">
+//       {books.map((book) => {
+//         return <Book key={book.id} book={book}></Book>;
+//       })}
+//     </section>
+//   );
+// }
+
+// This method uses the spread operator!
+// spreads out all the properties in the book!
 function BookList() {
   return (
     <section className="booklist">
-      <Book
-        img={firstBook.img}
-        title={firstBook.title}
-        author={firstBook.author}
-      />
-      <Book
-        img={secondBook.img}
-        title={secondBook.title}
-        author={secondBook.author}
-      />
+      {books.map((book) => {
+        return <Book key={book.id} {...book}></Book>;
+      })}
     </section>
   );
 }
 
 // React, props is short for properties!
 
-const Book = (props) => {
+const Book = ({ img, title, author }) => {
+  // let's say that I don't want to write props, I could do this...
+  // const {img, title, author} = props
+  // You'd replace props above with ({})
+  // children prop is a unchanged name but useful if you don't define a prop name.
+
+  // const { img, title, author } = props.book;
   return (
     <article className="book">
-      <img src={props.img} alt="bookcover" />
-      <h1> {props.title} </h1>
-      <h4>{props.author}</h4>
+      <img src={img} alt="bookcover" />
+      <h1> {title} </h1>
+      <h4>{author}</h4>
     </article>
   );
 };
