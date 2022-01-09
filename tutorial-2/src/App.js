bcd5b40646bcadd0cee612325dcf93cc7e2e3610
Bhatia, Ronak K <ronak.k.bhatia@accenture.com>
2022-01-08 16:52:56 -0800

    Finished the part on conditional rendering

--- tutorial-2/src/App.js
+++ tutorial-2/src/App.js
@@ -1,9 +1,8 @@
 import React from "react";
-import Setup from "./tutorial/2-useEffect/setup/3-useEffect-fetch-data";
+import Setup from "./tutorial/4-forms/setup/1-controlled-inputs";
 function App() {
   return (
     <div className="container">
-      {/* <h2>Advanced Tutorial</h2> */}
       <Setup />
     </div>
   );
--- tutorial-2/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
+++ tutorial-2/src/tutorial/3-conditional-rendering/setup/2-short-circuit.js
@@ -1,12 +1,37 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 // short-circuit evaluation
 // ternary operator
 
 const ShortCircuit = () => {
-  // const firstValue = text || 'hello world';
-  // const secondValue = text && 'hello world';
+  const [text, setText] = useState("");
+  const [isError, setIsError] = useState(false);
+  const firstValue = text || "hello world"; // if no text, returns helloworld
+  const secondValue = text && "hello world"; // if true, returns hw, if not, text
 
-  return <h2>short circuit</h2>;
+  // JSX has to return a value/expression.
+
+  return (
+    <>
+      {/* <h1> {firstValue}</h1>
+      <h1>value: {secondValue} </h1> */}
+      {/* <h1>{text || "John Doe"} </h1> */}
+      {/* {text && <h1> hello world </h1>} */}
+      <h1> {text || "Testing"} </h1>
+      <button className="btn" onClick={() => setIsError(!isError)}>
+        {" "}
+        Toggle Error{" "}
+      </button>
+      {isError && <h1> Error... </h1>}
+      {isError ? (
+        <p> There is an error... </p>
+      ) : (
+        <div>
+          {" "}
+          <h2> There is no Error </h2>
+        </div>
+      )}
+    </>
+  );
 };
 
 export default ShortCircuit;
